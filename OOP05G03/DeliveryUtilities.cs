34d8a266168e918d0072da3a2ac88e0703decffd
passant ahmed <passantahmed992006@outlook.com>
2026-09-23 23:32:34 +0300

    Q7

---
+++ OOP05G03/DeliveryUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP05G03
+{
+    internal static class DeliveryUtilities
+    {
+        public static void PrintSeparator()
+        {
+        }
+        public static void PrintSystemTitle()
+        {
+        }
+        public static void PrintSystemTitle(string title)
+        {
+            PrintSeparator();
+            Console.WriteLine(title);
+            PrintSeparator();
+        }
+    }
+}
