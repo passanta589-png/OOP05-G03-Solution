30916a8e68be36e4eb35a4515f2b6c1c30f18b78
passant ahmed <passantahmed992006@outlook.com>
2026-09-23 23:35:53 +0300

    Q9

---
+++ OOP05G03/ShipmentS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP05G03
+{
+    internal partial class Shipment
+    {
+        public string TrackingStatus { get; set; }
+
+        partial void OnTrackingStatusChanged(string newStatus);
+
+        public void UpdateTrackingStatus(string newStatus)
+        {
+            TrackingStatus = newStatus;
+            OnTrackingStatusChanged(newStatus);
+        }
+        partial void OnTrackingStatusChanged(string newStatus)
+        {
+            Console.WriteLine($"Tracking status changed to: {newStatus}");
+        }
+    }
+}
