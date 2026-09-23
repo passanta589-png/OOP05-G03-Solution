856c382686d41fe8e93030d99cbea3556136d3f1
passant ahmed <passantahmed992006@outlook.com>
2026-09-23 23:31:35 +0300

    Q6

---
+++ OOP05G03/DeliveryAddress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP05G03
+{
+    internal class DeliveryAddress
+    {
+        public string City { get; set; }
+        public DeliveryAddress(string city)
+        {
+            city = city;
+        }
+        public DeliveryAddress copy()
+        {
+            return new DeliveryAddress(this.City);
+        }
+    }
+}
---
+++ OOP05G03/Shipment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP05G03
+{
+    internal partial class Shipment
+    {
+        public string TrackingCode { get; set; }
+        public string ShipmentType { get; set; }
+        public double Weight { get; set; }
+        public DeliveryAddress Address { get; set; }
+        public static int TotalShipmentsCreated;
+        static Shipment()
+        {
+            TotalShipmentsCreated = 0;
+            Console.WriteLine("Shipment System Initialized");
+        }
+        public Shipment(string TrackingCode, string shipmentType, double Weight, string addressString)
+        {
+            this.TrackingCode = TrackingCode;
+            this.ShipmentType = shipmentType;
+            this.Weight = Weight;
+            this.Address = new DeliveryAddress(addressString);
+        }
+        public static int GetTotalShipmentsCreated()
+        {
+            return TotalShipmentsCreated;
+        }
+        public Shipment CopyShipment()
+        {
+            return this;
+        }
+        public Shipment ShallowCopy()
+        {
+            return (Shipment)this.MemberwiseClone();
+        }
+        public Shipment DeepCopy()
+        {
+            Shipment copy = (Shipment)this.MemberwiseClone();
+            copy.Address = this.Address.copy();
+            return copy;
+        }
+    }
+}
