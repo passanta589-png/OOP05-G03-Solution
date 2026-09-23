df62f1ffde8eb427046cc68f82a72dda678cc53c
passant ahmed <passantahmed992006@outlook.com>
2026-09-23 23:33:34 +0300

    Q8

---
+++ OOP05G03/ShipmentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP05G03
+{
+    internal class ShipmentExtensions
+    {
+        public static string GetSummary(this Shipment shipment)
+        {
+            return $"{shipment.TrackingCode} | {shipment.ShipmentType} | {shipment.Weight} KG | {shipment.TrackingStatus}";
+        }
+
+        public static bool IsDelivered(this Shipment shipment)
+        {
+            return shipment.TrackingStatus.Equals("Delivered", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
